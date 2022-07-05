fc37665293950278f97dadd84f4c51256d57be6b
od3ng <noprianto@polinema.ac.id>
2022-07-05 21:54:20 +0700

    add fps

--- face-testing-cnn.py
+++ face-testing-cnn.py
@@ -15,6 +15,13 @@ from keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
 cap = cv2.VideoCapture(0)
+
+start_time = time.time()
+# FPS update time in seconds
+display_time = 2
+fc = 0
+FPS = 0
+
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 model = load_model("save_model.h5")
 cv2.namedWindow("face testing", cv2.WINDOW_GUI_EXPANDED)
@@ -29,9 +36,23 @@ width, height = 160, 160
 
 while True:
     ret, frame = cap.read()
+
+    fc += 1
+    TIME = time.time() - start_time
+
+    if TIME >= display_time:
+        FPS = fc / TIME
+        fc = 0
+        start_time = time.time()
+
+    fps_disp = "FPS: " + "{:.2f}".format(FPS)
+
     if frame is None:
         print("No frame")
         continue
+
+        # Add FPS count on frame
+    cv2.putText(frame, fps_disp, (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(30, 30))
@@ -52,7 +73,7 @@ while True:
             print("index {}, name is {}".format(y_class, dirs[y_class]))
             text = "{} {}%".format(dirs[y_class], "{:.2f}".format(y_confidence))
             print("text {}".format(text))
-            cv2.putText(frame, text, (int(x), int(y - 5)), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+            cv2.putText(frame, text, (int(x), int(y - 5)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1)
 
     cv2.imshow("face testing", frame)
     time.sleep(0.25)
